7fd1c499e2f19cade9de322c46d0acc8e80ec635
PalakNagar2506 <palak.nagar@somaiya.edu>
2026-02-04 12:40:54 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ def home():
     return render_template_string("""
     <h1>Hello Flask 👋</h1>
     <p>This is a simple Flask app.</p>
+    <h2>I am making changes in this, Are the differences visible?"</h2>
     """)
 
 if __name__ == "__main__":
